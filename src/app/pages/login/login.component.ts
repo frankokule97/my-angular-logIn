276dfb4b87588e9d7fd465a90a6f45ff32491bef
Franko Kulešević <franko@Frankos-MacBook-Pro-2.local>
2025-01-09 20:41:57 +0100

    chore: adding logic for login and local storage on registration, problem with router injection (commented)

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {Router} from 'express';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  isLoginView: boolean = true;
+
+  userRegisterObj: any = {
+    userName:'',
+    password:'',
+    emailId:''
+  }
+
+  userLoginObj: any = {
+    userName:'',
+    password:'',
+  }
+
+  // router = inject(Router);
+
+  onLogin() {
+    const isLocalData = localStorage.getItem('angular18Local');
+    if(isLocalData != null){
+      const users = JSON.parse(isLocalData);
+
+      const isUserFound = users.find((m: any) => m.userName ==this.userLoginObj.userName && m.userPassword == this.userLoginObj.password);
+      if(isUserFound != undefined){
+        // this.router.navigateByUrl('dashboard');
+      } else {
+        alert("User name or password is Wrong");
+      }
+    } else {
+      alert("No User Found");
+    }
+  }
+
+  onRegister(){
+    const isLocalData = localStorage.getItem('angular18Local');
+    if(isLocalData != null){
+      const localArray = JSON.parse(isLocalData);
+      localArray.push(this.userRegisterObj);
+      localStorage.setItem("angular18Local",JSON.stringify(localArray));
+    } else {
+      const localArray = [];
+      localArray.push(this.userRegisterObj);
+      localStorage.setItem("angular18Local",JSON.stringify(localArray));
+    }
+    alert("Registration Success!");
+  }
+
 
 }
